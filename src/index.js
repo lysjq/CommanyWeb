7f383fe6539b20a47198ac1e1457769a1d3c17b5
lysjq <741779880@qq.com>
2018-10-28 22:22:38 +0800

    完成网页，增加过度效果

--- src/index.js
+++ src/index.js
@@ -1,35 +1,31 @@
-import { get2000Date } from "./data";
-import { eletrics } from './eledata';
 new WOW().init();
-let tbody = $('#table-tbody');
-Object.keys(eletrics).forEach(ele => {
-    let tr = document.createElement('tr');
-    let text = `<td>${ele}</td><td>${eletrics[ele].name}</td><td>${eletrics[ele].seekId}</td>`;
-    tr.innerHTML = text;
-    tbody.append(tr);
-    if (ele.includes('开关')) {
-        tr.className = 'success';
-    } else if (ele.includes('插座')) {
-        tr.className = 'info';
-    } else {
-        tr.className = '';
+
+let progress1 = $('#progess1');
+let width = 0;
+let timer = setInterval(function(){
+    if (width >= 100) {
+        width = 0;
     }
-});
-let data2000 = get2000Date();
-for (const ele of data2000) {
-    let tr = document.createElement('tr');
-    let text = `<td>${ele}</td><td>${ele.name}</td><td>${ele.seekId}</td>`;
-    tr.innerHTML = text;
-    tbody.append(tr);
-    if (ele.name.includes('开关')) {
-        tr.className = 'success';
-    } else if (ele.name.includes('插座')) {
-        tr.className = 'info';
+    if (width < 25) {
+        progress1.removeClass('bg-danger');
+        progress1.addClass('bg-dark');
+    } else if (width < 50) {
+        progress1.removeClass('bg-dark');
+        progress1.addClass('bg-success');
+    } else if (width < 75) {
+        progress1.removeClass('bg-success');
+        progress1.addClass('bg-warning');
     } else {
-        tr.className = '';
+        progress1.removeClass('bg-warning');
+        progress1.addClass('bg-danger');
     }
+    width++;
+    progress1.width(width + '%');
+    progress1.text(`${width}-运营研究社ing...`);
+}, 100);
 
-}
-
-
-
+$('.card').addClass('wow fadeInUp');
+$('.card').attr('data-wow-duration', '2.5s');
+$('p').addClass('wow fadeInLeft');
+$('h4').addClass('wow fadeInRight');
+$('.list-group').children().addClass('wow fadeInUp');
